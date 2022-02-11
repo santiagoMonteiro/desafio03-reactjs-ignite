2b421c573712facfb964f5ade9eb31668a6692e1
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-02-11 16:56:03 -0400

    the addProduct function has been defined

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,13 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,20 +29,58 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const productInCart = cart.find((product) => product.id === productId);
+
+      const responseOfProductInStock = await api.get(`stock/${productId}`);
+      const productAmountInStock = responseOfProductInStock.data.amount;
+
+      if (productInCart) {
+        const productAmountInCart = productInCart.amount;
+
+        if (productAmountInStock - productAmountInCart > 0) {
+          productInCart.amount += 1;
+
+          const newCart = cart.map((product) => {
+            if (product.id === productId) {
+              return productInCart;
+            }
+            return product;
+          });
+
+          setCart(newCart);
+        } else {
+          toast.error("Quantidade solicitada fora de estoque");
+        }
+      } else {
+        if (productAmountInStock >= 0) {
+          const responseOfProductInProductsRoute = await api.get(
+            `products/${productId}`
+          );
+
+          const newProduct = {
+            ...responseOfProductInProductsRoute.data,
+            amount: 1,
+          };
+
+          setCart([...cart, newProduct]);
+        } else {
+          toast.error("Quantidade solicitada fora de estoque");
+        }
+      }
+
     } catch {
-      // TODO
+      toast.error("Erro na adição do produto");
     }
   };
 
